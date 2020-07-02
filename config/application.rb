beffcdca5c41ecea3b68e30e3ec33b1f1a9aaf3c
Denis Neverov <denis.neverov@gmail.com>
2020-07-03 02:34:33 +0300

    Add Projects

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 module ProjectManagement
   class Application < Rails::Application
-    config.middleware.use Apartment::Elevators::Subdomain
+    # config.middleware.use Apartment::Elevators::Subdomain
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_215902) do
+ActiveRecord::Schema.define(version: 2020_07_02_230831) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
